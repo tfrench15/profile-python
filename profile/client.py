b948dcb776185ba4eab2e5ad4fdb77c27ae448a3
Tim French <tfrench15@gmail.com>
2019-10-27 16:31:25 -0700

    add docstrings

--- profile/client.py
+++ profile/client.py
@@ -3,7 +3,7 @@ import requests
 import exceptions
 
 class Client(object):
-
+    """Client represents a client to Segment's Profile API."""
     base_url = 'https://profiles.segment.com/v1/spaces/'
 
     def __init__(self, namespace, secret):
@@ -11,6 +11,8 @@ class Client(object):
         self.secret = secret
 
     def get_traits(self, id, value, include=None, verbose=None, limit=None): 
+        """get_traits makes requests against the /traits endpoint, returning a profile's traits."""
+
         url = self.base_url + self.namespace + '/collections/users/profiles/' + id + ':' + value + '/traits'
         params = {}
         if include:
@@ -31,6 +33,7 @@ class Client(object):
     def get_events(self, id, value, include=None, 
                    exclude=None, start=None, end=None, 
                    limit=None, sort=None):
+        """get_events makes requests against the /events endpoint, returning a profile's events."""
 
         url = self.base_url + self.namespace + '/collections/users/profiles/' + id + ':' + value + '/events'
         params = {}
@@ -59,7 +62,8 @@ class Client(object):
 
     def get_external_ids(self, id, value, verbose=None, 
                          include=None, limit=None):
-        
+        """get_external_ids makes requests against the /external_ids endpoint, returning a profile's external IDs."""
+
         url = self.base_url + self.namespace + '/collections/users/profiles/' + id + ':' + value + '/external_ids'
         params = {}
 
@@ -79,6 +83,8 @@ class Client(object):
         return res.json()
 
     def get_metadata(self, id, value, verbose=None):
+        """get_metadata makes requests against the /metadata endpoint, returning a profile's metadata."""
+
         url = self.base_url + self.namespace + '/collections/users/profiles/' + id + ':' + value + '/metadata'
         params = {}
 
@@ -89,6 +95,8 @@ class Client(object):
         return res.json()
 
     def get_links(self, id, value):
+        """get_links makes requests against the /links endpoint, returning a profile's links."""
+
         url = self.base_url + self.namespace + '/collections/users/profiles/' + id + ':' + value + '/links'
 
         res = requests.get(url, auth=(self.secret, ''))
