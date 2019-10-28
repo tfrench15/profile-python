bb54202b406ceb59b794d7a6f7c463e5747e2523
Tim French <tfrench15@gmail.com>
2019-10-27 18:23:15 -0700

    add docstrings to exceptions

--- profile/exceptions.py
+++ profile/exceptions.py
@@ -1,12 +1,14 @@
 
 
 class LimitError(Exception):
+    """ LimitError is raised when bad input is passed to the limit parameter. """
     def __init__(self, limit, message):
         self.limit = limit
         self.message = message
 
     
 class SortError(Exception):
+    """ SortError is raised when bad input is passed to the sort parameter. """
     def __init__(self, sort, message):
         self.sort = sort
         self.message = message
